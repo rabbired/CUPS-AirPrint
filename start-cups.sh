24918cd5314f08e140685c9ac1eaba5a770eb696
rabbired <rabbired@outlook.com>
2018-06-02 10:25:12 +0800

    setup

--- start-cups.sh
+++ start-cups.sh
@@ -3,8 +3,8 @@ set -e
 set -x
 
 if [ $(grep -ci $CUPS_USER_ADMIN /etc/shadow) -eq 0 ]; then
-    adduser -H -S -G lpadmin $CUPS_USER_ADMIN 
-    echo $CUPS_USER_ADMIN:$CUPS_USER_PASSWORD | chpasswd
+	useradd --groups=sudo,lp,lpadmin --create-home --home-dir=/home/$CUPS_USER_ADMIN --shell=/bin/bash $CUPS_USER_ADMIN
+	echo $CUPS_USER_ADMIN:$CUPS_USER_PASSWORD | chpasswd
 fi
 
 exec /usr/sbin/cupsd -f
